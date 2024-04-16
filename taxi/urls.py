96adbe54737caf6e27836ebf0a8147f1f7793a7a
vital <vitaliyburkalo81@gmail.com>
2024-04-16 13:40:51 +0300

    Fixed paths

--- taxi/urls.py
+++ taxi/urls.py
@@ -13,9 +13,9 @@ urlpatterns = [
     path("manufacturers/", ManufacturerListView.as_view(),
          name="manufacturer-list"),
     path("cars/", CarListView.as_view(), name="car-list"),
-    path("cars/<int:pk>", CarDetailView.as_view(), name="car-detail"),
+    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path("drivers/<int:pk>", DriverDetailView.as_view(), name="driver-detail"),
+    path("drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"),
 ]
 
 app_name = "taxi"
